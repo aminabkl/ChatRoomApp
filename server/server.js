23d61d2a458ee163518ea676104797f48b14ca7a
Amina <aminabakkali2001@gmail.com>
2023-06-15 15:41:42 +0100

    fixed cors error

--- server/server.js
+++ server/server.js
@@ -1,51 +1,25 @@
-// const express = require("express");
-// const http = require("http");
-// const cors = require("cors");
-// const WebSocket = require("ws");
-
-// const port = 6969;
-// const app = express();
-// app.use(cors());
-// const server = http.createServer(app);
-// const WebSocketServer = new WebSocket.Server({ server });
-
-// WebSocketServer.on("headers", (headers, request) => {
-// 	headers.push("Access-Control-Allow-Origin: *");
-// 	headers.push(
-// 		"Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"
-// 	);
-// });
-
-// WebSocketServer.on("connection", function connection(ws) {
-// 	ws.on("message", function incoming(data) {
-// 		WebSocketServer.clients.forEach(function each(client) {
-// 			if (client !== ws && client.readyState === WebSocket.OPEN) {
-// 				client.send(data);
-// 			}
-// 		});
-// 	});
-// });
-
-// server.listen(port, function () {
-// 	console.log(`Server is listening on ${port}!`);
-// });
-
-const WebSocket = require("ws");
 const http = require("http");
+const express = require("express");
+// const { Server } = require("socket.io");
+const cors = require("cors");
 
-const server = http.createServer();
-const wss = new WebSocket.Server({ server });
+const app = express();
 
-wss.on("connection", (ws) => {
-	ws.on("message", (message) => {
-		wss.clients.forEach((client) => {
-			if (client !== ws && client.readyState === WebSocket.OPEN) {
-				client.send(message);
-			}
-		});
-	});
+app.use(cors()); // Enable CORS with the provided options
+const server = http.createServer(app);
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000", // Allow requests from this origin
+		methods: ["GET", "POST"], // Allow specific HTTP methods
+		credentials: true, // Enable credentials (e.g., cookies, authorization headers)
+	},
+});
+const PORT = 8000;
+
+io.on("connection", (socket) => {
+	console.log(socket.id);
 });
 
-server.listen(8080, () => {
-	console.log("Server started on port 8080");
+server.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
 });
