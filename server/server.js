79578239da50ef0c3142a92792f37d91900b4a08
Amina <aminabakkali2001@gmail.com>
2023-06-15 19:52:00 +0100

    dynamic chat

--- server/server.js
+++ server/server.js
@@ -1,24 +1,39 @@
 const http = require("http");
 const express = require("express");
-// const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
 
-app.use(cors()); // Enable CORS with the provided options
+app.use(cors());
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "http://localhost:3000", // Allow requests from this origin
-		methods: ["GET", "POST"], // Allow specific HTTP methods
-		credentials: true, // Enable credentials (e.g., cookies, authorization headers)
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"],
+		credentials: true,
 	},
 });
 const PORT = 8000;
 
-io.on("connection", (socket) => {
-	console.log(socket.id);
-});
+let socketsConnected = new Set();
+
+io.on("connection", onConnected);
+function onConnected(socket) {
+	console.log("CON => ", socket.id);
+	socketsConnected.add(socket.id);
+
+	io.emit("client-total", socketsConnected.size);
+
+	socket.on("disconnect", () => {
+		console.log("DISCON => ", socket.id);
+		socketsConnected.delete(socket.id);
+		io.emit("client-total", socketsConnected.size);
+	});
+	socket.on("message", (data) => {
+		console.log(data);
+		socket.broadcast.emit("chat-message", data);
+	});
+}
 
 server.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
