f2ac9252ec489ed95d1c60254522bd9d9f729c73
Amina <aminabakkali2001@gmail.com>
2023-06-15 14:31:33 +0100

    ws working client-side

--- server/server.js
+++ server/server.js
@@ -1,21 +1,51 @@
-const express = require("express");
-const http = require("http");
-const WebSocket = require("ws");
+// const express = require("express");
+// const http = require("http");
+// const cors = require("cors");
+// const WebSocket = require("ws");
 
-const port = 8000;
-const server = http.createServer(express);
+// const port = 6969;
+// const app = express();
+// app.use(cors());
+// const server = http.createServer(app);
+// const WebSocketServer = new WebSocket.Server({ server });
+
+// WebSocketServer.on("headers", (headers, request) => {
+// 	headers.push("Access-Control-Allow-Origin: *");
+// 	headers.push(
+// 		"Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"
+// 	);
+// });
+
+// WebSocketServer.on("connection", function connection(ws) {
+// 	ws.on("message", function incoming(data) {
+// 		WebSocketServer.clients.forEach(function each(client) {
+// 			if (client !== ws && client.readyState === WebSocket.OPEN) {
+// 				client.send(data);
+// 			}
+// 		});
+// 	});
+// });
+
+// server.listen(port, function () {
+// 	console.log(`Server is listening on ${port}!`);
+// });
+
+const WebSocket = require("ws");
+const http = require("http");
+
+const server = http.createServer();
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", function connection(ws) {
-	ws.on("message", function incoming(data) {
-		wss.clients.forEach(function each(client) {
+wss.on("connection", (ws) => {
+	ws.on("message", (message) => {
+		wss.clients.forEach((client) => {
 			if (client !== ws && client.readyState === WebSocket.OPEN) {
-				client.send(data);
+				client.send(message);
 			}
 		});
 	});
 });
 
-server.listen(port, function () {
-	console.log(`Server is listening on ${port}!`);
+server.listen(8080, () => {
+	console.log("Server started on port 8080");
 });
