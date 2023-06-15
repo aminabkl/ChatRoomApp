f6023ddc4fb20b7670d354e62fa4e57798324b40
Amina <aminabakkali2001@gmail.com>
2023-06-15 12:21:21 +0100

    project init

---
+++ server/app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+// const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_USERNAME}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+
+// mongoose
+// 	.connect(uri)
+// 	.then(() => {
+// 		console.log("Successfully connected to MongoDB Atlas!");
+// 	})
+// 	.catch((error) => {
+// 		console.log("Unable to connect to MongoDB Atlas!");
+// 		console.error(error);
+// 	});
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	);
+	next();
+});
+
+module.exports = app;
