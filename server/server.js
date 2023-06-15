fb884eb5231a0db03e889a1eba168f5592827b46
Amina <aminabakkali2001@gmail.com>
2023-06-15 12:46:35 +0100

    ws init

--- server/server.js
+++ server/server.js
@@ -1,48 +1,21 @@
+const express = require("express");
 const http = require("http");
-const app = require("./app");
+const WebSocket = require("ws");
 
-const normalizePort = (val) => {
-	const port = parseInt(val, 10);
+const port = 8000;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({ server });
 
-	if (isNaN(port)) {
-		return val;
-	}
-	if (port >= 0) {
-		return port;
-	}
-	return false;
-};
-const port = normalizePort(process.env.PORT || "8000");
-app.set("port", port);
-
-const errorHandler = (error) => {
-	if (error.syscall !== "listen") {
-		throw error;
-	}
-	const address = server.address();
-	const bind =
-		typeof address === "string" ? "pipe " + address : "port: " + port;
-	switch (error.code) {
-		case "EACCES":
-			console.error(bind + " requires elevated privileges.");
-			process.exit(1);
-			break;
-		case "EADDRINUSE":
-			console.error(bind + " is already in use.");
-			process.exit(1);
-			break;
-		default:
-			throw error;
-	}
-};
-
-const server = http.createServer(app);
-
-server.on("error", errorHandler);
-server.on("listening", () => {
-	const address = server.address();
-	const bind = typeof address === "string" ? "pipe " + address : "port " + port;
-	console.log("Listening on " + bind);
+wss.on("connection", function connection(ws) {
+	ws.on("message", function incoming(data) {
+		wss.clients.forEach(function each(client) {
+			if (client !== ws && client.readyState === WebSocket.OPEN) {
+				client.send(data);
+			}
+		});
+	});
 });
 
-server.listen(port);
+server.listen(port, function () {
+	console.log(`Server is listening on ${port}!`);
+});
